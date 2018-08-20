bf013560994923f18382038fff47c3af7cda34e1
Emily Asch <e.asch@me.com>
2018-08-20 15:44:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,17 @@
 # Write your code here.
-# def line(katz_deli)
-# new_arr = []
-# count = 0 
-
-# if katz_deli.length == 0 
-# puts "The line is empty."
-
-# else 
-# katz_deli.each do |name|
-
-# new_arr << ("#{count += 1}. #{name}")
-
-# end
-# end
-# return "The line is currently: #{new_arr.join(" ")}"
-# end
-
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    message="The line is currently:"
+new_arr = []
+count = 0 
 
-    katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
-    end
+if katz_deli.length == 0 
+puts "The line is empty."
 
-    puts "#{message}"
-  end
+else 
+katz_deli.each do |name|
 
+new_arr << ("#{count += 1}. #{name}")
 
+end
+end
+return "The line is currently: #{new_arr.join(" ")}"
 end
