a63ebc6f731b52983e76b8325a6e8168acd97ba0
Emily Asch <e.asch@me.com>
2018-08-20 15:37:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,16 @@
 # Write your code here.
-def line (katz_deli)
-  
-  if katz_deli.length ==  0 
-  puts "The line is currently empty."
-  else 
-    
- 
-  end 
+def line(katz_deli)
+new_arr = []
+count = 0 
+
+if katz_deli == 0 
+return "The line is currently empty."
+else 
+katz_deli.each do |name|
+
+new_arr << ("#{count += 1}. #{name}")
+
+end
+end
+return "The line is currently: #{new_arr.join(" ")}"
 end
